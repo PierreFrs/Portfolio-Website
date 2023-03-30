9ae490b41d4c6703b0f2d1ac0d63f06a7de9bc58
Pierre <pierrefraisse90@gmail.com>
2023-03-30 18:54:32 +0200

    function toggleLightMode

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,34 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+// Light/dark theme
+const btnToggle = document.querySelector('.toggle-label');
+const btnInput = document.querySelector('.toggle-input');
+
+btnToggle.addEventListener('click', () => {
+
+  const body = document.body;
+
+  const toggleLightMode = () => {
+    if (body.classList.contains("dark")) {
+    // Toggle light mode if body class = .dark
+    body.classList.add('light');
+    body.classList.remove('dark');
+    document.documentElement.style.setProperty('--bg-color', '#fcfcfc');
+    document.documentElement.style.setProperty('--primary-color', '#000000');
+    document.documentElement.style.setProperty('--card-color', '#fcfcfc');
+    
+  } else if (body.classList.contains("light")) {
+    // Toggle dark mode if body class = .light
+    body.classList.add('dark');
+    body.classList.remove('light');
+    document.documentElement.style.setProperty('--bg-color', '#000000');
+    document.documentElement.style.setProperty('--primary-color', '#fcfcfc');
+    document.documentElement.style.setProperty('--card-color', '#474747');
+  }
+}
+  toggleLightMode();
+})
+
+// reset light/dark class when reloading
